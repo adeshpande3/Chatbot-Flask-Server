46fb8ef49c800b4088bb8eca0239b9ba824272dd
Adit Deshpande <adeshpande3@g.ucla.edu>
2017-07-23 15:11:25 -0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,67 @@
+import numpy as np
+import pickle
+import tensorflow as tf
+from flask import Flask, jsonify, render_template, request
+import model
+
+# Load in data structures
+with open("data/wordList.txt", "rb") as fp:
+    wordList = pickle.load(fp)
+wordList.append('<pad>')
+wordList.append('<EOS>')
+
+# Load in hyperparamters
+vocabSize = len(wordList)
+batchSize = 24
+maxEncoderLength = 15
+maxDecoderLength = 15
+lstmUnits = 48
+numLayersLSTM = 3
+
+# Create placeholders
+encoderInputs = [tf.placeholder(tf.int32, shape=(None,)) for i in range(maxEncoderLength)]
+decoderLabels = [tf.placeholder(tf.int32, shape=(None,)) for i in range(maxDecoderLength)]
+decoderInputs = [tf.placeholder(tf.int32, shape=(None,)) for i in range(maxDecoderLength)]
+feedPrevious = tf.placeholder(tf.bool)
+
+encoderLSTM = tf.nn.rnn_cell.BasicLSTMCell(lstmUnits, state_is_tuple=True)
+#encoderLSTM = tf.nn.rnn_cell.MultiRNNCell([singleCell]*numLayersLSTM, state_is_tuple=True)
+decoderOutputs, decoderFinalState = tf.contrib.legacy_seq2seq.embedding_rnn_seq2seq(encoderInputs, decoderInputs, encoderLSTM, 
+                                                            vocabSize, vocabSize, lstmUnits, feed_previous=feedPrevious)
+
+decoderPrediction = tf.argmax(decoderOutputs, 2)
+
+# Start session and get graph
+sess = tf.Session()
+#y, variables = model.getModel(encoderInputs, decoderLabels, decoderInputs, feedPrevious)
+
+# Load in pretrained model
+saver = tf.train.Saver()
+saver.restore(sess, tf.train.latest_checkpoint('models'))
+zeroVector = np.zeros((1), dtype='int32')
+
+def pred(inputString):
+    inputVector = model.getTestInput(inputString, wordList, maxEncoderLength)
+    feedDict = {encoderInputs[t]: inputVector[t] for t in range(maxEncoderLength)}
+    feedDict.update({decoderLabels[t]: zeroVector for t in range(maxDecoderLength)})
+    feedDict.update({decoderInputs[t]: zeroVector for t in range(maxDecoderLength)})
+    feedDict.update({feedPrevious: True})
+    ids = (sess.run(decoderPrediction, feed_dict=feedDict))
+    return model.idsToSentence(ids, wordList)
+
+# webapp
+app = Flask(__name__)
+
+
+@app.route('/prediction', methods=['POST', 'GET'])
+def prediction():
+    response =  pred(str(request.json['message']))
+    return jsonify(response)
+
+@app.route('/')
+def main():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ model.py
@@ -0,0 +1,37 @@
+import tensorflow as tf 
+import numpy as np 
+
+def getTestInput(inputMessage, wList, maxLen):
+	encoderMessage = np.full((maxLen), wList.index('<pad>'), dtype='int32')
+	inputSplit = inputMessage.lower().split()
+	for index,word in enumerate(inputSplit):
+		try:
+			encoderMessage[index] = wList.index(word)
+		except ValueError:
+			continue
+	encoderMessage[index + 1] = wList.index('<EOS>')
+	encoderMessage = encoderMessage[::-1]
+	encoderMessageList=[]
+	for num in encoderMessage:
+		encoderMessageList.append([num])
+	return encoderMessageList
+
+def idsToSentence(ids, wList):
+    EOStokenIndex = wList.index('<EOS>')
+    padTokenIndex = wList.index('<pad>')
+    myStr = ""
+    listOfResponses=[]
+    for num in ids:
+        if (num[0] == EOStokenIndex or num[0] == padTokenIndex):
+            listOfResponses.append(myStr)
+            myStr = ""
+        else:
+            myStr = myStr + wList[num[0]] + " "
+    if myStr:
+        listOfResponses.append(myStr)
+    listOfResponses = [i for i in listOfResponses if i]
+    listOfResponses = list(set(listOfResponses))
+    #chosenString = ''.join(listOfResponses)
+    chosenString = listOfResponses[0]
+    #chosenString = max(listOfResponses, key=len)
+    return chosenString
